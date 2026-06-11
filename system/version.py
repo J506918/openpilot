2d30780d3438487bf65669573f46045a1110463b
Wuying Created Local Users <admin@rwqs3hhu6ml3lms.CN-HANGZHOU-198-13-1.WUYING.LOCAL>
2026-06-12 02:19:17 +0800

    chore: add J506918 repos to sunnypilot_remote whitelist

--- system/version.py
+++ system/version.py
@@ -107,7 +107,9 @@ class OpenpilotMetadata:
     return self.git_normalized_origin in ("github.com/sunnypilot/sunnypilot",
                                           "github.com/sunnypilot/openpilot",
                                           "github.com/sunnyhaibin/sunnypilot",
-                                          "github.com/sunnyhaibin/openpilot")
+                                          "github.com/sunnyhaibin/openpilot",
+                                          "github.com/J506918/sunnypilot",
+                                          "github.com/J506918/openpilot")
 
   @property
   def git_normalized_origin(self) -> str:
